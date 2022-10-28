4933e03455c82a7c4f4e380670093e51039cf89c
brickwall <sk292207@gmail.com>
2022-10-28 12:00:48 +0530

    Run rubocop through code

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 class Word
   attr_accessor :word
 
@@ -13,7 +12,7 @@ class Word
 
   def get_a_word
     words = fetch_words
-    while word == nil
+    while word.nil?
       @word = words.sample
       @word = nil unless valid?(word)
     end
@@ -25,7 +24,6 @@ class Word
   end
 end
 
-
 require 'open-uri'
 require 'json'
 require 'colorize'
@@ -35,10 +33,9 @@ class Meaning < Word
   attr_accessor :meaning
 
   def meaning
-      data = URI.open("https://api.dictionaryapi.dev/api/v2/entries/en/#{get_a_word}").read
-      parsed_data = JSON.parse(data)
-      meaning = parsed_data[0]["meanings"][0]["definitions"][0]["definition"]
-      meaning
+    data = URI.open("https://api.dictionaryapi.dev/api/v2/entries/en/#{get_a_word}").read
+    parsed_data = JSON.parse(data)
+    parsed_data[0]['meanings'][0]['definitions'][0]['definition']
   end
 end
 
@@ -60,40 +57,41 @@ class Gameplay < Meaning
   end
 
   def repeat?(character)
-    true if guesses.any?{|char| char.uncolorize == character }
+    true if guesses.any? { |char| char.uncolorize == character }
   end
 
   def get_char
-    puts "Enter a character"
+    puts 'Enter a character'
     char = gets.chomp
-    unless (char.match(/[a-z]/) && char.length == 1)
-      puts "Enter a valid input"
-      get_char
-    else
+    if char.match(/[a-z]/) && char.length == 1
       char
+    else
+      puts 'Enter a valid input'
+      get_char
     end
   end
 
   def welcome
-    print "Welcome to a variation of Hangman\nNote: 20-30% of time, it fails to fetch the meaning, so rerun in that case\n "
+    print "Welcome to a variation of Hangman\nNote: 20-30% of time, it fails to fetch the
+     meaning, so rerun in that case\n "
   end
 
   def hint
     dictionary = Meaning.new
     begin
       puts "Hint: #{dictionary.meaning}"
-    rescue
+    rescue StandardError
       retry
     end
     dictionary.word
-  end 
+  end
 
   def instruction
-    puts "You have 10 guesses to determine the word"
+    puts 'You have 10 guesses to determine the word'
   end
 
   def casket
-    @word_casket = @word.split('').map{|chars| '_'}
+    @word_casket = @word.split('').map { |_chars| '_' }
     display_casket
   end
 
@@ -103,24 +101,23 @@ class Gameplay < Meaning
   end
 
   def show_guesses
-    guesses.each{|guess| print guess}
+    guesses.each { |guess| print guess }
     puts
   end
 
   def update_casket(char)
-    word_casket.each_index do |index| 
+    word_casket.each_index do |index|
       word_casket[index] = char if word[index] == char
     end
     display_casket
   end
 
   def display_casket
-    word_casket.each{|char| print char}
+    word_casket.each { |char| print char }
     puts
   end
 
   def play(word)
-
     (1..10).each do |index|
       puts "Attempt #{index}"
       guessed_char = input
@@ -128,13 +125,12 @@ class Gameplay < Meaning
       show_guesses
       update_casket(guessed_char)
       if word_casket.join == word
-        puts "You did it"
+        puts 'You did it'
         break
       end
     end
     puts "Better luck next time, the word was #{word}" if word_casket.join != word
   end
-
 end
 
 hangman = Gameplay.new
